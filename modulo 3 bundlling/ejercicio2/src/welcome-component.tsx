cc613889181cc5d86cc8476a17942179edb41941
Luis Garrido Ogalla <ogalla23@gmail.com>
2025-03-02 21:23:19 +0100

    add new change to exercise 1 and exercise 2

---
+++ modulo 3 bundlling/ejercicio2/src/welcome-component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './mystyles.scss';
+export const WelcomeComponent: React.FC = () => {
+  return (
+    <div>
+      <h1 className = "red-background">Hello world from React!</h1>
+    </div> 
+  );
+};
