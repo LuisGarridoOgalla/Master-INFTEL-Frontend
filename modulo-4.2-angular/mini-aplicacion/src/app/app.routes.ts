c9aabf2941f174c3f950c9b0687103c27423793d
Luis Garrido Ogalla <ogalla23@gmail.com>
2025-12-23 16:20:39 +0100

    update routes file

--- modulo-4.2-angular/mini-aplicacion/src/app/app.routes.ts
+++ modulo-4.2-angular/mini-aplicacion/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home';
-import { LoginComponent } from './pages/login/login';
-import { AboutComponent } from './pages/about/about';
-import { GalleryComponent } from './pages/gallery/gallery';
-import { DashboardComponent } from './pages/dashboard/dashboard';
-import { ProfileComponent } from './pages/profile/profile';
+import {HomeComponent} from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
