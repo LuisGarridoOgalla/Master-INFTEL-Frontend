ed52027d687d8d9eacc197633b0d434403a1c321
Luis Garrido Ogalla <ogalla23@gmail.com>
2025-12-24 13:45:23 +0100

    add header behaviour with logout and add localStorage for credentials

--- modulo-4.2-angular/mini-aplicacion/src/app/services/auth.service.ts
+++ modulo-4.2-angular/mini-aplicacion/src/app/services/auth.service.ts
@@ -6,6 +6,19 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class AuthService {
 
+  constructor() {
+    let savedUsername = '';
+    let savedIsLogged = 'false';
+
+    if (typeof window !== 'undefined' && window.localStorage) {
+      savedUsername = localStorage.getItem('username') || '';
+      savedIsLogged = localStorage.getItem('isLogged') || 'false';
+    }
+    if (savedUsername && savedIsLogged === 'true') {
+      this.isLoggedSubject.next(true);
+      this.usernameSubject.next(savedUsername);
+    }
+  }
   private isLoggedSubject = new BehaviorSubject<boolean>(false);
   private usernameSubject = new BehaviorSubject<string>('');
 
@@ -16,16 +29,19 @@ export class AuthService {
     if (credentials.username === 'admin' && credentials.password === 'admin123') {
       this.isLoggedSubject.next(true);
       this.usernameSubject.next(credentials.username);
-      
+      localStorage.setItem('username', credentials.username);
+      localStorage.setItem('isLogged', 'true');
       return true;
     }
-    
+
     return false;
   }
 
   logout(): void {
     this.isLoggedSubject.next(false);
     this.usernameSubject.next('');
+    localStorage.removeItem('username');
+    localStorage.removeItem('isLogged');
   }
 
   isLogged(): boolean {
